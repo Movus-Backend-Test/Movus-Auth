2953d8bd6cc59026a912f5578bb0513440154c04
advistasyam <advis.tasyah@ui.ac.id>
2021-07-25 13:52:58 +0700

    add MVP jwt function

---
+++ auth-app/main/functions/mailer.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer')
+const transporter = nodemailer.createTransport({
+    pool: true,
+    host: process.env.SMTP_HOST,
+    port: 587,
+    tls: {
+        secureProtocol: "TLSv1_method"
+    },
+    auth: {
+      user: 'no-reply@advistm.tech',
+      pass: process.env.SMTP_PASSWORD
+    }
+})
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error(error)
+  }
+  else {
+    console.log('Server is ready to take messages')
+  }
+})
+
+async function email(data) {
+  transporter.sendMail(data, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+}
+
+module.exports = email
---
+++ auth-app/main/functions/tokenAuth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+
+const verifyToken = async (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        console.log(req.user)
+        next()
+    })
+}
+
+const generateToken = async (data) => {
+    return jwt.sign(data, process.env.TOKEN_SECRET, { expiresIn: '1h' })
+}
+
+module.exports = {
+    verify: verifyToken,
+    generate: generateToken,
+}
